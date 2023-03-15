4183be2acac81f060035da1928a3b7808df0b3eb
vodangkhoa2001 <vdk0610@gmail.com>
2023-03-15 13:04:19 +0700

    Tìm hiểu và sử dụng thư viện Axios

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -4,10 +4,12 @@ import { useState, useEffect, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 
+import * as searchServices from '~/apiServices/searchServices'
 import { Wrapper as PopperWrapper } from '~/components/Popper'
 import AccountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icons';
-import styles from './Search.module.scss'
+import styles from './Search.module.scss';
+import { useDebounce } from '~/hooks';
 
 const cx = classNames.bind(styles)
 function Search() {
@@ -16,22 +18,23 @@ function Search() {
     const [showResult, setShowResult] = useState(true)
     const [loading, setLoading] = useState(false)
 
+    const debouced = useDebounce(searchValue, 700)
+
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debouced.trim()) {
             setSearchResult([])
             return;
         }
-        setLoading(true)
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
-            .then((res) => res.json())
-            .then((res) => {
-                setSearchResult(res.data);
-                setLoading(false)
-            })
-            .catch(() =>
-                setLoading(false)
-            )
-    }, [searchValue]);
+        const fecthApi = async () => {
+            setLoading(true)
+
+            const result = await searchServices.search(debouced)
+            setSearchResult(result)
+
+            setLoading(false)
+        }
+        fecthApi()
+    }, [debouced]);
     const inputRef = useRef()
 
     const handleClear = () => {
