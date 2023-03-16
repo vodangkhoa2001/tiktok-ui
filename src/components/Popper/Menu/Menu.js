a17fe8fe71b7748c7ed480a3920662c48af154bf
vodangkhoa2001 <vdk0610@gmail.com>
2023-03-16 13:18:31 +0700

    Giải quyết vấn đề nhiều file index.js

---
+++ src/components/Popper/Menu/Menu.js
@@ -0,0 +1,70 @@
+import PropTypes from 'prop-types'
+import Tippy from "@tippyjs/react/headless";
+import classNames from "classnames/bind";
+import { Wrapper as PopperWrapper } from '~/components/Popper'
+
+import MenuItem from "./MenuItem";
+import Header from "./Header";
+import styles from './Menu.module.scss'
+import { useState } from "react";
+
+const cx = classNames.bind(styles)
+const defaultFunc = () => { }
+function Menu({ children, items = [], hideOnClick = false, onChange = defaultFunc }) {
+    const [history, setHistory] = useState([{ data: items }])
+    const current = history[history.length - 1]
+
+
+    const renderItem = () => {
+        return current.data.map((item, index) => {
+            const isParent = !!item.children
+            return <MenuItem
+                key={index}
+                data={item}
+                onClick={() => {
+                    if (isParent) {
+                        setHistory(prev => [...prev, item.children])
+                    } else {
+                        onChange(item)
+                    }
+                }} />
+        })
+    }
+    return (
+        <Tippy
+            interactive
+            offset={[12, 8]}
+            delay={[0, 700]}
+            hideOnClick={hideOnClick}
+            placement='bottom-end'
+            render={attrs => (
+                <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
+                    <PopperWrapper className={cx('menu-popper')}>
+                        {history.length > 1 &&
+                            (<Header
+                                title={current.title}
+                                onBack={() => {
+                                    setHistory((prev) => prev.slice(0, prev.length - 1))
+                                }}
+                            />
+                            )}
+                        <div className={cx('menu-body')}>{renderItem()}</div>
+                    </PopperWrapper>
+                </div>
+            )}
+            onHide={() => {
+                setHistory(prev => prev.slice(0, 1))
+            }}
+        >
+            {children}
+        </Tippy>
+    );
+}
+Menu.propTypes = {
+    children: PropTypes.node.isRequired,
+    items: PropTypes.array,
+    hideOnClick: PropTypes.bool,
+    onChange: PropTypes.func,
+}
+
+export default Menu;
---
+++ src/routes/routes.js
@@ -0,0 +1,25 @@
+import config from '~/config'
+// Layouts
+import { HeaderOnly } from '~/layouts'
+
+
+// Pages
+import Home from '~/pages/Home'
+import Following from '~/pages/Following'
+import Profile from '~/pages/Profile'
+import Upload from '~/pages/Upload'
+import Search from '~/pages/Search'
+
+// Public Routes
+export const publicRoutes = [
+    { path: config.routes.home, compnent: Home },
+    { path: config.routes.following, compnent: Following },
+    { path: config.routes.profile, compnent: Profile },
+    { path: config.routes.upload, compnent: Upload, layout: HeaderOnly },
+    { path: config.routes.search, compnent: Search, layout: null },
+]
+
+// Private Routes
+export const privateRoutes = [
+
+]
